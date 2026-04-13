b048eb26b0c53c13b9b965f31ae13e3b4cfdf96f
Freddy Campos <marthianfred@gmail.com>
2026-04-13 02:28:59 -0400

    fix(departamentos): resolve JwtService dependency error by importing AuthModule

--- src/modules/departamentos/departamentos.module.ts
+++ src/modules/departamentos/departamentos.module.ts
@@ -3,9 +3,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Departamento } from '../../entities/departamento.entity';
 import { DepartamentosService } from './departamentos.service';
 import { DepartamentosController } from './departamentos.controller';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Departamento])],
+  imports: [
+    TypeOrmModule.forFeature([Departamento]),
+    AuthModule,
+  ],
   controllers: [DepartamentosController],
   providers: [DepartamentosService],
   exports: [DepartamentosService],
